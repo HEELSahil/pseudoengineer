ec79730407267411a831e41e126b82a3a3a6c3e0
Sahil Kotian <sahil.kotian316@gmail.com>
2023-06-22 01:53:21 +0530

    Added Layout for Notes Page

--- app/notes/page.jsx
+++ app/notes/page.jsx
@@ -1,9 +1,26 @@
+import notesData from '@data/notesData'
+import NotesCard from '@components/NotesCard'
+
 export default function Page(){
     return(
-        <div className="flex justify-center pt-52 pb-2 text-4xl md:text-6xl lg:text-7xl font-thin md:font-semibold lg:font-bold underline bg-gradient-to-r bg-clip-text  text-transparent 
-        from-lime-500 via-emerald-500 to-lime-500
-        animate-text">
-            Notes Page
-        </div>
+        <section className="divide-y divide-gray-200 dark:divide-gray-700">
+            <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+                <h1 className="pb-2 text-4xl md:text-6xl sm:leading-10 md:leading-14 leading-9 tracking-tight font-thin md:font-bold lg:font-extrabold bg-gradient-to-r bg-clip-text text-transparent from-lime-500 via-emerald-500 to-lime-500 animate-text">
+                    Download Course Notes 
+                </h1>
+            </div>
+            <div className="container py-12">
+                <div className="-m-4 flex flex-wrap">
+                    {notesData.map((d) => (
+                        <NotesCard
+                            key={d.title}
+                            title={d.title}
+                            imgSrc={d.imgSrc}
+                            href={d.href}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
---
+++ data/notesData.js
@@ -0,0 +1,19 @@
+const notesData = [
+    {
+        title: 'MERN Stack',
+        imgSrc: '/static/images/mern.png',
+        href: 'https://www.google.com',
+    },
+    {
+        title: 'Flutter',
+        imgSrc: '/static/images/flutter2.jpg',
+        href: 'https://www.google.com',
+    },
+    {
+        title: 'Tailwind CSS',
+        imgSrc: '/static/images/tailwind.jpg',
+        href: 'https://www.google.com',
+      },
+  ]
+  
+  export default notesData
