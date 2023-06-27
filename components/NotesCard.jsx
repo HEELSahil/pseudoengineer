9524903c2fda1823afd080466f6ff58b8e9d3faa
Sahil Kotian <sahil.kotian316@gmail.com>
2023-06-28 00:30:47 +0530

    Fixed Responsiveness of Cards on Large Screens

--- components/NotesCard.jsx
+++ components/NotesCard.jsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const NotesCard = ({ title, imgSrc, href }) => (
-    <div className="p-4 md:w-1/3" style={{ maxWidth: '544px' }}>
+    <div className="p-4 md:w-1/2 xl:w-1/3" style={{ maxWidth: '544px' }}>
         <div class="h-full w-full rounded-2xl bg-gradient-to-r from-indigo-400 via-purple-400 to-indigo-400 shadow-2xl p-1">
             <div class="flex h-full w-full items-center justify-center rounded-2xl bg-white dark:bg-zinc-900 transform transition-transform duration-200 hover:scale-105">
                 <div className={`${ imgSrc && 'h-full'} overflow-hidden`}>
                     <Image
                         alt={title}
                         src={imgSrc}
-                        className="object-cover object-center rounded-t-2xl md:h-36 lg:h-48"
+                        className="object-cover object-center rounded-t-2xl md:h-48"
                         width={544}
                         height={306}
                     />
