b73fa72cf430ba8f910cc7e71066c656baed7a51
Sahil Kotian <sahil.kotian316@gmail.com>
2024-06-26 13:01:21 -0400

    Updated Social Media Links

--- data/notesData.js
+++ data/notesData.js
@@ -2,17 +2,17 @@ const notesData = [
     {
         title: 'MERN Stack',
         imgSrc: '/static/images/mern.png',
-        href: 'https://www.google.com',
+        href: 'https://www.linkedin.com/company/pseudoengineer',
     },
     {
         title: 'Flutter',
         imgSrc: '/static/images/flutter2.jpg',
-        href: 'https://www.google.com',
+        href: 'https://www.linkedin.com/company/pseudoengineer',
     },
     {
         title: 'Tailwind CSS',
         imgSrc: '/static/images/tailwind.jpg',
-        href: 'https://www.google.com',
+        href: 'https://www.linkedin.com/company/pseudoengineer',
       },
   ]
   
