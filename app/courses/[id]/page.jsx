aacf29f388900c62650aa8934e74711d14d79637
Sahil Kotian <sahil.kotian316@gmail.com>
2024-06-28 10:41:55 -0400

    Fixed Deployment Error where React Hook was being called Conditionally

--- app/courses/[id]/page.jsx
+++ app/courses/[id]/page.jsx
@@ -5,18 +5,17 @@ import { coursesList } from '@data/coursesList';
 import CoursesCard from '@components/CoursesCard';
 
 export default function CourseList({ params }) {
-    const slug = String(params.id);
-    const course = coursesList.find((course) => course.slug === slug);
+  const [searchTerm, setSearchTerm] = useState('');
+  const slug = String(params.id);
+  const course = coursesList.find((course) => course.slug === slug);
 
-    if (!course) {
-        return <div>Course not found</div>;
-    }
+  const handleSearch = (e) => {
+      setSearchTerm(e.target.value);
+  };
 
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleSearch = (e) => {
-        setSearchTerm(e.target.value);
-    };
+  if (!course) {
+      return <div>Course not found</div>;
+  }
 
     const problems = Object.values(course.folder);
 
