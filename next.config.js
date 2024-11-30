0963cf5627401cd100a821e8312657b0970456c2
Sahil Kotian <sahil.kotian316@gmail.com>
2024-11-29 20:18:50 -0500

    Readded metadataBase URL Configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  metadata: {
-    metadataBase: new URL(
-      process.env.NODE_ENV === 'production'
-        ? 'https://www.pseudoengineer.dev'
-        : 'http://localhost:3000'
-    )
-  }
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'x-metadata-base',
+            value: process.env.NODE_ENV === 'production'
+              ? 'https://www.pseudoengineer.dev'
+              : 'http://localhost:3000'
+          }
+        ]
+      }
+    ];
+  }
+};
