5a431ee2a7a9031ca44637034748094ed5e46792
Sahil Kotian <sahil.kotian316@gmail.com>
2024-11-29 20:04:23 -0500

    Added metadataBase URL Configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  metadata: {
+    metadataBase: new URL(
+      process.env.NODE_ENV === 'production'
+        ? 'https://www.pseudoengineer.dev'
+        : 'http://localhost:3000'
+    )
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
