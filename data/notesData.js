fa671fff370ae6e572474607bc237e2a2d798839
Sahil Kotian <sahil.kotian316@gmail.com>
2024-11-30 19:52:38 -0500

    Added Download Functionality For Course Notes

--- data/notesData.js
+++ data/notesData.js
@@ -2,17 +2,17 @@ const notesData = [
     {
         title: 'MERN Stack',
         imgSrc: '/static/images/mern.png',
-        href: 'https://www.linkedin.com/company/pseudoengineer',
+        href: 'https://drive.google.com/uc?export=download&id=1sMfgbgqAPFRU3g0aB73X2RWcX0sZ6Ci_',
     },
     {
         title: 'Flutter',
         imgSrc: '/static/images/flutter2.jpg',
-        href: 'https://www.linkedin.com/company/pseudoengineer',
+        href: 'https://drive.google.com/uc?export=download&id=1sMfgbgqAPFRU3g0aB73X2RWcX0sZ6Ci_',
     },
     {
         title: 'Tailwind CSS',
         imgSrc: '/static/images/tailwind.jpg',
-        href: 'https://www.linkedin.com/company/pseudoengineer',
+        href: 'https://drive.google.com/uc?export=download&id=1sMfgbgqAPFRU3g0aB73X2RWcX0sZ6Ci_',
       },
   ]
   
