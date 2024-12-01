e20cc759ef152bde59ac4f15b32fe55e9adc9748
Sahil Kotian <sahil.kotian316@gmail.com>
2024-12-01 14:05:59 -0500

    Updated Content For The About Page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,11 +8,11 @@ export default function Page() {
     return (
         <>
             <div className="space-y-2 py-8 md:space-y-5">
-                <div className="w-5/6 md:w-3/4 p-6 border-2 border-gray-300 rounded-3xl">
+                <div className="w-5/6 p-6 border-2 border-gray-300 rounded-3xl">
                     <div className="pb-2 text-3xl md:text-5xl font-extrabold lg:font-extrabold underline bg-gradient-to-r bg-clip-text text-transparent from-sky-500 via-indigo-500 to-sky-500 animate-text">
                         Who is a pseudoEngineer?
                     </div>
-                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-4">A pseudoEngineer is anyone who, at some point, pretends to know a particular skill or technology more than they actually do. This could be you, me, or any engineer who adds skills to their resume they don&apos;t fully master, either to stand out in the competitive job market or to secure an interview. Often, the realization of lacking complete knowledge comes when preparing for an interview.</p>
+                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-3">A pseudoEngineer is anyone who, at some point, pretends to know a particular skill or technology more than they actually do. This could be you, me, or any engineer who adds skills to their resume they don&apos;t fully master, either to stand out in the competitive job market or to secure an interview. Often, the realization of lacking complete knowledge comes when preparing for an interview. Now, with the emergence of AI tools like ChatGPT, many of us have fallen into the habit of simply doing <strong>&#96;Ctrl + C, Ctrl + V&#96;</strong> without truly understanding the logic behind the code.</p>
                 </div>
             </div>
             <div className="flex space-y-2 py-2 md:space-y-5 justify-end">
@@ -20,7 +20,7 @@ export default function Page() {
                     <div className="pb-2 text-3xl md:text-5xl font-extrabold lg:font-extrabold underline bg-gradient-to-r bg-clip-text text-transparent from-sky-500 via-indigo-500 to-sky-500 animate-text">
                         Why do we do this?
                     </div>
-                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-4">The intense competition in today&apos;s job market drives us to present ourselves as the best among our peers. We list skills that we fully know, partially know, or even skills we&apos;re struggling to understand, hoping to land a job and then scramble to prepare adequately once an opportunity arises.</p>
+                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-4">The intense competition in today&apos;s job market drives us to present ourselves as the best among our peers. We list skills that we either don&apos;t know at all, have some grasp of, or even skills we&apos;re struggling to understand, hoping to land a job and then quickly brush up once an opportunity arises.</p>
                 </div>
             </div>
             <div className="space-y-2 py-8 md:space-y-5">
@@ -28,31 +28,7 @@ export default function Page() {
                     <div className="pb-2 text-3xl md:text-5xl font-extrabold lg:font-extrabold underline bg-gradient-to-r bg-clip-text text-transparent from-sky-500 via-indigo-500 to-sky-500 animate-text">
                         How will pseudoEngineer help?
                     </div>
-                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-4">pseudoEngineer is a platform designed to simplify complex programming technologies and make learning accessible for these pseudoEngineers. Here&apos;s how: </p>
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" strokeWidth="1" stroke="currentColor" className="w-9 h-9 text-gray-500 dark:text-gray-400">
-                            <path fillRule="evenodd" d="M15.28 9.47a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 1 1-1.06-1.06L13.69 10 9.97 6.28a.75.75 0 0 1 1.06-1.06l4.25 4.25ZM6.03 5.22l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L8.69 10 4.97 6.28a.75.75 0 0 1 1.06-1.06Z" clipRule="evenodd" />
-                        </svg>
-                        <p className="text-gray-500 dark:text-gray-400 text-lg pt-1 md:pb-4">Simplified Programming Languages: Clear, straightforward explanations and practical exercises to make programming languages easy to grasp.</p>
-                    </div>
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" strokeWidth="1" stroke="currentColor" className="w-7 h-7 text-gray-500 dark:text-gray-400">
-                            <path fillRule="evenodd" d="M15.28 9.47a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 1 1-1.06-1.06L13.69 10 9.97 6.28a.75.75 0 0 1 1.06-1.06l4.25 4.25ZM6.03 5.22l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L8.69 10 4.97 6.28a.75.75 0 0 1 1.06-1.06Z" clipRule="evenodd" />
-                        </svg>
-                        <p className="text-gray-500 dark:text-gray-400 text-lg md:pb-4">Web Development Made Easy: Comprehensive courses that break down web development into manageable steps.</p>
-                    </div>
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" strokeWidth="1" stroke="currentColor" className="w-7 h-7 text-gray-500 dark:text-gray-400">
-                            <path fillRule="evenodd" d="M15.28 9.47a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 1 1-1.06-1.06L13.69 10 9.97 6.28a.75.75 0 0 1 1.06-1.06l4.25 4.25ZM6.03 5.22l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L8.69 10 4.97 6.28a.75.75 0 0 1 1.06-1.06Z" clipRule="evenodd" />
-                        </svg>
-                        <p className="text-gray-500 dark:text-gray-400 text-lg md:pb-4">Straightforward System Design: Practical approaches and real-world examples to demystify system design.</p>
-                    </div>
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" strokeWidth="1" stroke="currentColor" className="w-6 h-6 text-gray-500 dark:text-gray-400">
-                            <path fillRule="evenodd" d="M15.28 9.47a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 1 1-1.06-1.06L13.69 10 9.97 6.28a.75.75 0 0 1 1.06-1.06l4.25 4.25ZM6.03 5.22l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L8.69 10 4.97 6.28a.75.75 0 0 1 1.06-1.06Z" clipRule="evenodd" />
-                        </svg>
-                        <p className="text-gray-500 dark:text-gray-400 text-lg md:pb-4">Coding Simplified: Techniques and tips to enhance coding skills without overwhelming learners.</p>
-                    </div>
+                    <p className="text-gray-500 dark:text-gray-400 text-lg pt-3 md:pb-3">pseudoEngineer will be a platform designed to simplify complex technologies with easy-to-understand explanations—as if teaching a 7-year-old. Your journey, from being a pseudoEngineer to donning the cape of mastery and becoming a superhero in the tech stack of your choice, starts here.</p>
                 </div>
             </div>
         </>
