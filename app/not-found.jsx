25a2c3e8f6d11de6ac56bb3665b5a26f6b184e72
Sahil Kotian <sahil.kotian316@gmail.com>
2025-04-19 12:02:59 -0400

    feat: add custom 404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center pt-20">
+      <img
+        src="/static/images/page-not-found.svg"
+        alt="Page Not Found"
+        className="w-full max-w-md"
+      />
+    </div>
+  );
+}
